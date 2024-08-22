d54ad9ee7dbdcb59d7853704f9d07014336579ce
Codgod21 <kingofcod112100@gmail.com>
2024-08-22 14:31:47 -0700

    coding done

--- W2/main.js
+++ W2/main.js
@@ -1,1 +1,17 @@
-console.log('AHHHHHHHH');
+
+const myHeading = doc.querySelector("h2")
+myHeading.textContent = '76';
+
+
+//Carbon footprint
+const myHouseMembers = 6;
+const myHouseSize = 4;
+const myFoodConsumption = 16;
+const myWaterUsage = 2;
+const myFurniture = 2;
+const myGarbage = 30;
+const myRecycling = 4;
+const myDriving = 12;
+const cpfTotal = myHouseMembers + myHouseSize + myFoodConsumption + myWaterUsage + myFurniture + myGarbage + myRecycling + myDriving;
+
+;
