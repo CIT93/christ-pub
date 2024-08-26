7c30295c8439efea49dde204821b595bae53ff21
Codgod21 <kingofcod112100@gmail.com>
2024-08-26 11:32:01 -0700

    Week 3 Attendance

--- W1/main.js
+++ W1/main.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+console.log("hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "AHHH";
+const myVarType = typeof(myVar);
+console.log("myVarType" + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log(`will this one run?`);
+    } else {
+        console.log(`Or will this one run?`);
+    };
+}
+runNow();
+
